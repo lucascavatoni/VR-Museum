3112eb92e7fd50ed3b06f905d878f40036858b86
lucascavatoni <63449960+lucascavatoni@users.noreply.github.com>
2022-10-02 20:13:23 +0200

    Add files via upload

---
+++ scripts/ButtonChange.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class ButtonChange : MonoBehaviour
+{
+    public GameObject ArseneClosed;
+    public GameObject ArseneOpened;
+
+    private InputDevice targetDevice;
+    private bool value0;
+    private bool value1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+
+        ArseneClosed.SetActive(true);
+        ArseneOpened.SetActive(false);
+
+        if(devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
+
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+        value0 = primaryButtonValue;
+    }
+
+
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+
+        value1 = primaryButtonValue;
+
+        if (value0 == false && value1 == true && ArseneClosed.activeInHierarchy)
+        {
+            ArseneClosed.SetActive(false);
+            ArseneOpened.SetActive(true);
+        }
+        else if (value0 == false && value1 == true && ArseneOpened.activeInHierarchy)
+        {
+            ArseneClosed.SetActive(true);
+            ArseneOpened.SetActive(false);
+        }
+
+        value0 = primaryButtonValue;
+    }
+}
